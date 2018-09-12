8e5b57d51d6a62d529ec3abf778fe762bc70e39d
feynman <feynman@dtu-5cg71662msv.win.dtu.dk>
2018-09-12 14:40:10 +0200

    Implemented filters and header files

--- filters.c
+++ filters.c
@@ -1,7 +1,30 @@
 #include "filters.h"
 
 // Feel free to change return statement and arguments
-void lowPassFilter()
-{
+int lowPassFilter(int data, int* unfilteredArray, int* filteredArray){
+float value = (2*filteredArray[0])-filteredArray[1]+((1.0/32.0)*(data-2*unfilteredArray[5]+unfilteredArray[11]));
+return (int) value;
+}
 
+int highPassFilter(int data,int* unfilteredArray, int* filteredArray){
+	float value = filteredArray[0]-data/32.0+unfilteredArray[15]-unfilteredArray[16]+unfilteredArray[31]/32.0;
+	return (int) value;
+}
+
+int derivative(int data, int* unfilteredArray){
+	float value = (1.0/8.0)*(2*data+unfilteredArray[0]-unfilteredArray[2]-2*unfilteredArray[3]);
+	return (int) value;
+}
+
+int squaring(int data){
+	return data*data;
+}
+
+int MWI(int data, int* unfilteredArray){
+	int sum = 0;
+	for(int i = 0; i < 32; i++){
+		sum += unfilteredArray[i];
+	}
+	sum += data;
+	return (int) (1.0/30.0)*sum;
 }
--- main.c
+++ main.c
@@ -12,12 +12,35 @@ int main()
 {	
     QRS_params qsr_params;       // Instance of the made avaiable through: #include "qsr.h"
 	FILE *file;                  // Pointer to a file object
-	file = openfile("ECG.txt");
+	file = openfile("ECG.txt");	//Open file ECG.txt
+    int* unfilteredDataArray = malloc(32*sizeof(int)); //Declare int array with 32 ints
+    int* filteredDataArray = malloc(2*sizeof(int)); //Declare int array with 2 ints
+
+
+    for(int i = 0; i < 32; i++){ //Set all indices to 0
+    	unfilteredDataArray[i] = 0;
+    }
+    for(int i = 0; i < 2; i++){ //Set all indices to 0
+        filteredDataArray[i] = 0;
+    }
+    for(int i = 0; i < 35; i++){
     int data = getNextData(file);          // Read Data from Sensor
 
-    //lowPassFilter();            // Filter Data
-                                
+    int LPF = lowPassFilter(data,unfilteredDataArray,filteredDataArray);  // Filter Data with lowpass
+    int HPF = highPassFilter(LPF,unfilteredDataArray,filteredDataArray);
+    int D = derivative(HPF,unfilteredDataArray);
+    int sq = squaring(D);
+    int mw = MWI(sq,unfilteredDataArray);
+
+    unfilteredDataArray = shuffleArray(unfilteredDataArray, 32, data);
+    filteredDataArray = shuffleArray(filteredDataArray,2 , mw);
+    }
     //peakDetection(&qsr_params); // Perform Peak Detection
 
+	return 0;
+}
+
+
+int* shuffleArray(int* array, int arrayCount, newData){
 	return 0;
 }
